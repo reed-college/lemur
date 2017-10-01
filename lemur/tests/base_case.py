73f7a103ebd7c66385f081ee8b4e4bef9a6cf514
Ross Donaldson <gastove@gmail.com>
2017-10-01 15:15:56 -0700

    Base test case for common configs

---
+++ lemur/tests/base_case.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+from lemur.lemur import app, db
+from lemur import models as m
+from db_populate import populate_db
+
+ds = db.session
+
+
+class LemurBaseCase(TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        # Create all the tables
+        db.create_all()
+        # Insert all the roles(Student, Admin, SuperAdmin) that will be used
+        m.Role.insert_roles()
+        # Populate db with objects
+        self.built_in_ids = populate_db()
+
+    # tidy up after a test has been run
+    def tearDown(self):
+        # remove and clean the database completely
+        ds.remove()
+        db.drop_all()
