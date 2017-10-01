0feec8eef16ac9b405cf9e951d1e11dae7e8ec6b
Ross Donaldson <gastove@gmail.com>
2017-10-01 13:19:35 -0700

    Configuration/bootstrapping revamp
    1. Move __main__ in to the app
    2. Rename __init__ to lemur.py; make a new init file
    3. Start moving to a slightly more robust configuration system

--- lemur/__main__.py
+++ lemur/__main__.py
@@ -1,8 +1,5 @@
-# TODO: This file should be moved under `lemur/`, and called from the root
-# project directory as: `python lemur` -- RMD 2017-08-26
-
 # This file is used to start the app
-from . import app
+import lemur
 
 if __name__ == '__main__':
-    app.run()
+    lemur.app.run()
